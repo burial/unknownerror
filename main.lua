0c5378e2076e6ecc884683d0802ce5fa9e1a11f3
burial <burialxyz@gmail.com>
2011-07-20 03:25:44 -0400

    init

---
+++ main.lua
@@ -0,0 +1,35 @@
+local mod = CreateFrame("frame", select(2, ...), UIParent)
+mod:SetScript("OnEvent", function(self, event, ...) return self[event](self, ...) end)
+mod:RegisterEvent("ADDON_LOADED")
+
+local lastError
+local filter = {
+  [ERR_INV_FULL] = true,
+  [ERR_SPELL_FAILED_ALREADY_AT_FULL_MANA] = true,
+  [ERR_SPELL_FAILED_ALREADY_AT_FULL_HEALTH] = true,
+  [ERR_PLAYER_WRONG_FACTION] = true,
+  [ERR_ARENA_TEAM_NAME_INVALID] = true,
+  [ERR_TAXIPLAYERALREADYMOUNTED] = true,
+}
+
+function mod:ADDON_LOADED(addon)
+  if addon == self:GetName() then
+    self:UnregisterEvent("ADDON_LOADED")
+    UIErrorsFrame:UnregisterEvent("UI_ERROR_MESSAGE")
+    self:RegisterEvent("UI_ERROR_MESSAGE")
+  end
+end
+
+function mod:UI_ERROR_MESSAGE(msg)
+  if filter[msg] then
+    UIErrorsFrame:AddMessage(msg, 1, 0, 0)
+  else
+    lastError = msg
+  end
+end
+
+SLASH_UE1 = "/ue"
+SLASH_UE2 = "/error"
+function SlashCmdList.UNKNOWNERROR()
+  UIErrorsFrame:AddMessage(lastError, 1, 0, 0)
+end
